4b015b425bd2f471b6965aa1850756919bb29759
RahulMk <rahulmk.94802@gmail.com>
2025-01-21 17:35:13 +0530

    EDSTEM project

--- src/main/java/com/edstem/serviceImpl/OrderServiceImpl.java
+++ src/main/java/com/edstem/serviceImpl/OrderServiceImpl.java
@@ -1,17 +1,19 @@
 package com.edstem.serviceImpl;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.edstem.enums.StatusEnum;
 import com.edstem.exception.OrderException;
 import com.edstem.model.MenuItem;
 import com.edstem.model.Order;
 import com.edstem.model.OrderItem;
-import com.edstem.repository.MenuItemRepository;
-import com.edstem.repository.OrderItemRepository;
 import com.edstem.repository.OrderRepository;
+import com.edstem.service.MenuItemService;
+import com.edstem.service.OrderItemService;
 import com.edstem.service.OrderService;
 
 @Service
@@ -21,24 +23,30 @@ public class OrderServiceImpl implements OrderService {
 	private OrderRepository orderRepository;
 
 	@Autowired
-	private MenuItemServiceImpl menuItemServiceImpl;
+	private MenuItemService menuItemService;
 
 	@Autowired
-	private OrderItemRepository orderItemRepository;
+	private OrderItemService orderItemService;
 
 	@Override
 	public Order createOrder(Order order) {
-		
-		BigDecimal toal= BigDecimal.ZERO;
-		
-//		for(OrderItem orderItem : order.getItems()) {
-//			MenuItem menuItem= menuItemServiceImpl.getItemById(orderItem.getMenuItem().getId());
-//			
-//			orderItem=menuItemServiceImpl.c
-//		}
-		return null;
-		
-		
+
+		BigDecimal total = BigDecimal.ZERO;
+
+		for (OrderItem item : order.getItems()) {
+			MenuItem menuItem = menuItemService.getItemById(item.getMenuItem().getId());
+
+			item = orderItemService.calculateSubTotalAmount(item, menuItem);
+			menuItemService.updateItem(menuItem);
+			total = total.add(item.getSubTotal());
+		}
+
+		order.setTotalAmount(total);
+		order.setStatus(StatusEnum.RECEIVED);
+		order.setCreatedAt(LocalDateTime.now());
+
+		return orderRepository.save(order);
+
 	}
 
 	@Override
